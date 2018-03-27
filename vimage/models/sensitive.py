d8e254f415a6830e40992394f54bb9a942a326c9
shawn <purpen.w@gmail.com>
2018-03-27 09:00:32 +0800

    add sensitive table

--- vimage/models/sensitive.py
+++ vimage/models/sensitive.py
@@ -8,12 +8,14 @@ __all__ = [
 
 
 class Sensitive(db.Model):
-    """敏感词库"""
+    """广告违禁词及敏感词库"""
 
     __tablename__ = 'sensitives'
 
     id = db.Column(db.Integer, primary_key=True)
     word = db.Column(db.String(32), nullable=False)
+    # 类型：1、广告违禁词；2、敏感词；
+    type = db.Column(db.SmallInteger, default=1)
 
     def __repr__(self):
         return '<Sensitive {}>'.format(self.word)
