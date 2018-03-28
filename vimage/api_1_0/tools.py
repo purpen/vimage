8670f842cd488106f7fb64b75d2d9f280e53cf0f
Fynn <yangpengfei@taihuoniao.com>
2018-03-28 22:13:54 +0800

    添加 pytesseract 测试 ocr 识别

--- vimage/api_1_0/tools.py
+++ vimage/api_1_0/tools.py
@@ -4,6 +4,14 @@ from flask import request, url_for, abort, g
 from . import api
 from vimage.models import Sensitive
 from vimage.helpers.utils import *
+from vimage.helpers.ocr import *
+
+
+@api.route('/tools/hello_ocr', methods=['GET'])
+def hello_ocr():
+    """测试 ocr 功能"""
+
+    return ocr_local_image()
 
 
 @api.route('/tools/pick_sensitive', methods=['POST'])
@@ -11,4 +19,6 @@ def pick_sensitive():
     """智能识别敏感词"""
     image_url = request.json.get('image')
 
+    # 测试
     # 读取图片，进行识别文本
+    return ocr_url_image(image_url)
