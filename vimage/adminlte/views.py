8bc98edb8fc1cb0faa172266f0df03e7c484b78c
shawn <purpen.w@gmail.com>
2018-04-08 23:41:44 +0800

    add feature

--- vimage/adminlte/views.py
+++ vimage/adminlte/views.py
@@ -7,5 +7,9 @@ from . import adminlte
 @adminlte.route('/')
 def dashboard():
     """管理控制台"""
-
-    return render_template('adminlte/dashboard.html')
+    features = [
+        '智能海报生成', '智能Banner生成', '社交图文生成', '图片添加二维码', '广告违禁词识别', '一键生成主图短视频', '智能图像压缩', '以图搜图比价',
+        '图像识别商品标注', '图片情感分析', '智能生成PPT', 'GIF动图制作'
+    ]
+    return render_template('adminlte/dashboard.html',
+                           features=features)
