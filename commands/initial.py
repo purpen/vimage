3af71375dd98eb7d3d7e6ec8d91ebf0a1953e392
pangqilong <1223750634@qq.com>
2018-10-18 15:38:04 +0800

    初始化铭感词汇数据

--- commands/initial.py
+++ commands/initial.py
@@ -1,6 +1,51 @@
 # -*- coding: utf-8 -*-
 
+import json
 from flask_script import Command
+
+from config import basedir
+from vimage.models import Sensitive
+from vimage import db
+
+
+def load_data(filename):
+    """加载json格式数据"""
+
+    try:
+        with open(filename, 'r', encoding="utf-8") as f:
+            result = json.load(f)
+            return result
+    except Exception as err:
+        print('读取文件异常: %s' % str(err))
+        return {}
+
+
+class InitialSystem(object):
+    """系统初始化，配置系统默认数据"""
+
+    @staticmethod
+    def init_sensitives():
+        """初始化敏感词汇"""
+
+        records = load_data(basedir + '/commands/sensitives.json')
+        for record in records:
+            word = record['word']
+            type = record['type']
+
+            sensitive = Sensitive.query.filter_by(word=word, type=type).first()
+            if sensitive:
+                print(f'System sensitive: {word} already initial, exit!')
+                continue
+
+            # 保存敏感词汇
+            sensitive = Sensitive()
+            sensitive.word = word
+            sensitive.type = type
+            db.session.add(sensitive)
+
+        db.session.commit()
+
+        print('System sensitive is initial!')
 
 
 class InitialSetting(Command):
@@ -9,4 +54,9 @@ class InitialSetting(Command):
     """
 
     def run(self):
-        print('Initial system data!')
+        print('Begin Initial system data!')
+
+        # 初始化敏感词汇
+        InitialSystem.init_sensitives()
+
+        print('End Initial system data!')
