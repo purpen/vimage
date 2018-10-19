96669f2e04a2c62d6f151a797cdf077a262ee1ce
shawn <purpen.w@gmail.com>
2018-10-19 21:46:15 +0800

    添加自动部署脚本

---
+++ sync_upgrade.example.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# 同步git代码库
+#
+# @example: /path/to/sync_upgrade.sh
+#
+
+unset GIT_DIR
+
+# 项目根目录
+PROJECT_ROOT="/opt/project/vimage"
+LOG_FILE="/opt/project/vimage/gitsync.log"
+
+echo -e "\n=================  `date +%Y-%m-%d\ %H:%M:%S`  ===============\n" >> $LOG_FILE 2>&1
+
+# 进入项目目录
+cd $PROJECT_ROOT
+
+# 这里直接丢弃工作区的内容，防止出现一些奇怪的错误。项目目录只做pull，不在这里修改东西
+git reset --hard
+
+# 先拉取，再合并
+git pull origin master
+
+
+echo "=========================== ok ===================="  >> $LOG_FILE 2>&1
